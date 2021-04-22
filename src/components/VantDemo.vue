723c3eb02246e40f6a98c0dee4239f9912848be9
liudong <liudong@zsoft.com.cn>
2021-04-22 09:11:08 +0800

    Initial commit

---
+++ src/components/VantDemo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="vantDemo">
+        <h1>Hello World</h1>
+        <van-cell-group>
+            <van-cell title="单元格" value="内容"></van-cell>
+            <van-cell title="单元格" value="内容"></van-cell>
+        </van-cell-group>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "VantDemo",
+    props: {
+        msg: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
